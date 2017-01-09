40d1bea272ca8675ba43a9c245eaa86ca59f3f49
peter-bannerflow <peter.bergman@bannerflow.com>
2017-01-09 11:40:51 +0100

    feat: Add JSON body parsing and a /proxy API route

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var port = process.env.PORT || 3000;
 var app = express();
+app.use(bodyParser.json());
+
 
 app.get('/test', function (req, res) {
   res.send('Hello World');
 })
+
+app.post('/proxy', function (req, res) {
+  console.log(req.body);
+  var baseUrl = req.body.baseUrl;
+  res.send('base URL: ' + baseUrl);
+});
 
 app.listen(port);
