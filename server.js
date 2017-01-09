fb67d688aa5c84e00a9a49acb48daa0393b530e4
peter-bannerflow <peter.bergman@bannerflow.com>
2017-01-09 12:39:57 +0100

    feat: Implement request forarding

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var port = process.env.PORT || 3000;
 var app = express();
@@ -10,10 +11,28 @@ app.get('/test', function (req, res) {
   res.send('Hello World');
 })
 
-app.post('/proxy', function (req, res) {
-  console.log(req.body);
-  var baseUrl = req.body.baseUrl;
-  res.send('base URL: ' + baseUrl);
+app.post('/proxy', function (proxyReq, proxyRes) {
+  var proxyHost = proxyReq.body.host;
+  var proxyPath = proxyReq.body.path;
+  var proxyHeaders = proxyReq.body.headers;
+
+  var options = {
+    host: proxyHost,
+    path: proxyPath,
+    headers: proxyHeaders,
+    timeout: 30000
+  };
+
+  var req = http.request(options, (res) => {
+    var responseData = '';
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      responseData += chunk;
+    });
+    res.on('end', () => {
+      proxyRes.send(responseData);
+    });
+  }).end();
 });
 
 app.listen(port);
